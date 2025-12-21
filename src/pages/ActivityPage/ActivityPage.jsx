34fdea9df324234dc39e5959ffa027b74de0730f
baraa <s12400192@stu.najah.edu>
2025-12-21 18:17:32 +0200

    add images from unsplash

--- src/pages/ActivityPage/ActivityPage.jsx
+++ src/pages/ActivityPage/ActivityPage.jsx
@@ -11,8 +11,8 @@ function Activities() {
             description: "استمتع بالمناظر الطبيعية أثناء التسلق على الجبال",
             date: "2025-12-20",
             images: [
-                "https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=800&q=80",
-                "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2VlfGVufDB8fDB8fHww",
+                "https://images.unsplash.com/photo-1550522667-09c9bdb293a1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2VlfGVufDB8fDB8fHww"
             ],
             location: "جبال القدس",
             participants: 15,
@@ -24,7 +24,7 @@ function Activities() {
             description: "تعلم أساسيات الرسم بالألوان المائية",
             date: "2025-12-21",
             images: [
-                "https://dayswithgrey.com/wp-content/uploads/2022/11/how-to-paint-with-kids-105.jpg"
+                "https://images.unsplash.com/photo-1629822908853-b1d2a39ece98?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGFpbnRpbmclMjBraWRzfGVufDB8fDB8fHww"
             ],
             location: "الدار الرئيسية",
             participants: 10,
@@ -36,7 +36,7 @@ function Activities() {
             description: "جلسة لتعليم الأطفال تلاوة القرآن",
             date: "2025-12-22",
             images: [
-                "https://jomalquran.b-cdn.net/wp-content/uploads/2023/02/quran-kids.jpg"
+                "https://images.unsplash.com/photo-1609599006353-e629aaabfeae?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cXVyYW58ZW58MHx8MHx8fDA%3D"
             ],
             location: "المسجد",
             participants: 20,
@@ -48,7 +48,7 @@ function Activities() {
             description: "تعلم العزف على آلات موسيقية مختلفة",
             date: "2025-12-23",
             images: [
-                "https://images.unsplash.com/photo-1511376777868-611b54f68947?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1723035092325-03ad613d3dae?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bXVzaWMlMjBhbmQlMjBraWRzfGVufDB8fDB8fHww"
             ],
             location: "الدار الرئيسية",
             participants: 12,
@@ -60,7 +60,7 @@ function Activities() {
             description: "جولة تعليمية للأطفال في متحف العلوم والتاريخ",
             date: "2025-12-24",
             images: [
-                "https://images.unsplash.com/photo-1581091012184-7b0f90d174a4?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1491156855053-9cdff72c7f85?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bXVzZXVtfGVufDB8fDB8fHww"
             ],
             location: "متحف المدينة",
             participants: 18,
@@ -72,7 +72,7 @@ function Activities() {
             description: "استمتع بالأنشطة المائية على شاطئ البحر",
             date: "2025-12-25",
             images: [
-                "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1439066615861-d1af74d74000?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHNlZXxlbnwwfHwwfHx8MA%3D%3D"
             ],
             location: "شاطئ المدينة",
             participants: 20,
@@ -84,7 +84,7 @@ function Activities() {
             description: "تعلم أساسيات الحياكة والخياطة",
             date: "2025-12-26",
             images: [
-                "https://images.unsplash.com/photo-1617196031971-779a34893026?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1578353022142-09264fd64295?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2V3aW5nfGVufDB8fDB8fHww"
             ],
             location: "الدار الرئيسية",
             participants: 8,
@@ -96,7 +96,7 @@ function Activities() {
             description: "أنشطة رياضية متنوعة للأطفال في الحديقة",
             date: "2025-12-27",
             images: [
-                "https://images.unsplash.com/photo-1599058917210-f0b99b9f0d06?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1486286701208-1d58e9338013?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vdGJhbGx8ZW58MHx8MHx8fDA%3D"
             ],
             location: "الحديقة العامة",
             participants: 25,
@@ -108,7 +108,7 @@ function Activities() {
             description: "نشاط فني لتشكيل وتلوين الطين",
             date: "2025-12-28",
             images: [
-                "https://images.unsplash.com/photo-1599086892842-78dc399c8a7f?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1762951045576-ae21990c2c46?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fENsYXklMjBjb2xvcmluZ3xlbnwwfHwwfHx8MA%3D%3D"
             ],
             location: "الدار الرئيسية",
             participants: 14,
@@ -120,7 +120,7 @@ function Activities() {
             description: "عرض مسرحي ترفيهي وتعليمي للأطفال",
             date: "2025-12-29",
             images: [
-                "https://images.unsplash.com/photo-1602810316733-3e99177d8f2b?auto=format&fit=crop&w=800&q=80"
+                "https://images.unsplash.com/photo-1710980350236-816833b63c4f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGElMjBwbGF5fGVufDB8fDB8fHww"
             ],
             location: "المسرح البلدي",
             participants: 30,
