f50c720977fd250811f1d06ee5d9bd9b50a29d8b
AliHmoudah <s12429286@stu.najah.edu>
2025-12-23 01:28:05 +0200

    Implement Header component and  add icons and some enhancements

--- src/dataOrphans/dataOrphans.jsx
+++ src/dataOrphans/dataOrphans.jsx
@@ -6,7 +6,8 @@ const orphansData = [
     gender: "انثى",
     city: "نابلس",
     country: "فلسطين",
-    
+    image:["https://vid.alarabiya.net/images/2018/07/16/430e04fe-7496-4c24-a674-b6ae35b859e8/430e04fe-7496-4c24-a674-b6ae35b859e8.jpg"]
+    ,
   },
   {
     id: 2,
@@ -15,7 +16,7 @@ const orphansData = [
     gender: "ذكر",
     city: "جنين",
     country: "فلسطين",
-    
+    image:["https://pbs.twimg.com/media/DqbDOe9WsAAx4rx.jpg"]
   },
   {
     id: 3,
@@ -24,6 +25,7 @@ const orphansData = [
     gender: "انثى",
     city: "نابلس",
     country: "فلسطين",
+    image:["https://www.rmaziaty.com/wp-content/uploads/2017/06/SSS%20(10).jpg"]
     
   }
 ];
