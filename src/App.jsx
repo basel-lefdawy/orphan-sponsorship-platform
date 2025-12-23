c09370f7e0d6485f92c74d1edb81fb8492828834
AliHmoudah <s12429286@stu.najah.edu>
2025-12-24 01:37:34 +0200

    Merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Activities from "./pages/ActivityPage/ActivityPage";
 import Orphan from './pages/orphans/orphan';
-import Details from './components/Details';  
+import Details from './components/Details';
 import HelpRequest from "./pages/HelpRequest/HelpRequest";
 import DonationForm from "./pages/Donation/Donation";
+import SponsoringPage from "./pages/sponsoring/sponsoring";
+import OrphansByCategoryPage from "./pages/sponsoring/OrphansByCategoryPage";
+import SponsorFormPage from "./pages/sponsoring/sponsoringForm/SponsorFormPage";
 
 function App() {
   return (
@@ -15,12 +18,22 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path='/activities' element={<Activities />} />
-        <Route path="/orphans" element={<Orphan/>} />
+        <Route path="/orphans" element={<Orphan />} />
         <Route path="/orphans/details/:id" element={<Details />} />
-        <Route path="/help" element={<HelpRequest/>} />
-        <Route path="/donate" element={<DonationForm/>} />
+        <Route path="/help" element={<HelpRequest />} />
+        <Route path="/donate" element={<DonationForm />} />
+        <Route path="/sponsoring" element={<SponsoringPage />} />
+        <Route
+          path="/sponsoring/:type"
+          element={<OrphansByCategoryPage />}
+        />
+        <Route
+          path="/sponsoring/details/:id"
+          element={<Details />}
+        />
+        <Route path="/SponsorFormPage" element={<SponsorFormPage />} />
 
-      </Route>     
+      </Route>
     </Routes>
   );
 }
