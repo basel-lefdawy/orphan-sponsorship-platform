82594bfa205229e0e90d610ec861b3aab86891fa
basel-lefdawy <blefdawy@gmail.com>
2025-12-24 01:13:57 +0200

    modifying the router and main

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,29 @@
-// src/routes/AppRoutes.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ActivityPage from '../pages/ActivityPage/ActivityPage';
-
+import { Routes, Route } from "react-router-dom";
+import ActivityPage from "../pages/ActivityPage/ActivityPage";
+import SponsoringPage from "../pages/sponsoring/sponsoring";
+import OrphansByCategoryPage from "../pages/sponsoring/OrphansByCategoryPage";
+import SponsorFormPage from "../pages/sponsoring/sponsoringForm/SponsorFormPage";
+import Details from "../pages/sponsoring/details";
 const AppRoutes = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/ActivityPage" element={<ActivityPage />} />
-            </Routes>
-        </Router>
+        <Routes>
+            {/* مثال: صفحة رئيسية ثانية للموقع */}
+            <Route path="/ActivityPage" element={<ActivityPage />} />
+
+            {/* قسم الكفالة */}
+            <Route path="/sponsoring" element={<SponsoringPage />} />
+            <Route
+                path="/sponsoring/:type"
+                element={<OrphansByCategoryPage />}
+            />
+            <Route path="/SponsorFormPage" element={<SponsorFormPage />} />
+            <Route
+                path="/details/:id"
+                element={<Details />}
+            />
+        </Routes>
     );
 };
 
 export default AppRoutes;
+
