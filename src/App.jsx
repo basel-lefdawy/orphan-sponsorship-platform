3129145dc254ca429743154b41c47449df151754
zaina-alkhuffash <zaina.khuffash123@gmail.com>
2025-12-23 22:17:11 +0200

    Connect Orphans page to app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
-
+import Orphan from './pages/orphans/orphan';
+import Details from './components/Details';  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
   return (
     <>
-    <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Orphan/>} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+        
+      </BrowserRouter>
+ 
     </>
   )
 }
