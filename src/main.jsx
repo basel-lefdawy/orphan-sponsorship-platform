940b32c1ce30f68ee918d45590becec2f98091a4
baraa <s12400192@stu.najah.edu>
2025-12-24 07:39:32 +0200

    fixing the deploy page

--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,7 @@ createRoot(document.getElementById("root")).render(
     <CacheProvider value={rtlCache}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <App />
         </BrowserRouter>
       </ThemeProvider>
