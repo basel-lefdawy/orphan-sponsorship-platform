21c2d874e4d8f777738db9131caf29b3e175cbd3
BaraaMasri99 <s12400192@stu.najah.edu>
2026-05-14 23:30:42 +0300

    updating the url to be suitable when we deploy on vercel

--- src/pages/HelpRequest/HelpRequest.jsx
+++ src/pages/HelpRequest/HelpRequest.jsx
@@ -28,7 +28,8 @@ import {
 const HelpRequest = () => {
 
   // BACKEND API
-  const API_URL = "http://localhost:5000/api/help-requests";
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  const API_URL = `${API_BASE_URL}/api/help-requests`;
 
   const {
     register,
